ac228a99b731d6d2d9e0329bd1263bd7d1786d73
mohammedrefat <mohammedrefat53@outlook.com>
2023-09-24 14:04:42 +0300

    task 0

--- _printf.c
+++ _printf.c
@@ -8,8 +8,25 @@
  */
 int _printf(const char *format, ...)
 {
+	int length = 0;
+	int specifier = 0;
+	int i;
 	int count = 0;
 	va_list arguments;
+	if (!format)
+	{
+		return (0);
+	}
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		length++;
+		if (format[i] == 37 && (format[i + 1]) != 37  && (format[i + 1] == 'c'  || format[i + 1] == 's' || 
+				format[i + 1] == 'i' || format[i + 1] == 'd'))
+		{
+			specifier++;
+		}
+	}
+	va_start(arguments, format);
 	while (*format)
 	{
 		if (*format == '%')
