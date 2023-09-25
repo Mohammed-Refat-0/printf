2ddb0f723fe9ceed01d3c7622d2a844c2d465627
mohammedrefat <mohammedrefat53@outlook.com>
2023-09-25 08:31:06 +0300

    task 1

--- _printf.c
+++ _printf.c
@@ -1,17 +1,24 @@
 #include "main.h"
 #include <unistd.h>
 #include <stdarg.h>
+#include <stdlib.h>
+#include <limits.h>
 /**
  * _printf - a custom build printf function
- * @ format: string to print with format specifier
+ * @format: string to print with format specifier
  * Return: number of chars printed
  */
 int _printf(const char *format, ...)
 {
+	int size = 0;
+	int number;
+	int holder;
+	int temp;
 	int length = 0;
 	int specifier = 0;
 	int i;
 	int count = 0;
+	char *ptr;
 	va_list arguments;
 	if (!format)
 	{
@@ -50,6 +57,66 @@ int _printf(const char *format, ...)
 				{
 				count = count + (write(1, string, 1));
 				string++;
+				}
+				format++;
+			}
+			else if (*format == 'd' || *format == 'i')
+			{
+				size = 0;
+				number = va_arg(arguments, int);
+				if (number >= 0)
+				{
+					temp = number;
+					while( temp >= 1)
+					{
+						temp = temp / 10;
+						size++;
+					}
+					ptr = malloc((size + 1) * sizeof(char));
+					if (!ptr)
+					{
+						return (1);
+					}
+					ptr[size] = '\0';
+					for (i = (size - 1); i >= 0; i--)
+					{
+						holder = number % 10;
+						number = number / 10;
+						ptr[i] = holder + '0';
+					}
+					while (*ptr)
+					{
+						count = count + (write(1, ptr, 1));
+						ptr++;
+					}
+				}
+				else
+				{
+					number = -number;
+					temp = number;
+					while (temp >= 1)
+					{
+						temp = temp / 10;
+						size++;
+					}
+					ptr = malloc((size + 2) * sizeof(char));
+					if (!ptr)
+					{
+						return (1);
+					}
+					ptr[size + 1] = '\0';
+					ptr[0] = '-';
+					for ( i = size; i > 0; i--)
+					{
+						holder = number % 10;
+						number = number / 10;
+						ptr[i] = holder + '0';
+					}
+					while (*ptr)
+					{
+						count = count + (write(1, ptr, 1));
+						ptr++;
+					}
 				}
 				format++;
 			}
